f69a7efe4a53283938a6a1872b5be5a3da9b33e1
Cynthia Villanueva <caqv426@yahoo.com>
2026-07-09 23:06:19 -0400

    JS2 M1 ASSIGNMENT CODES

---
+++ js/script.js
@@ -0,0 +1,96 @@
+// GET ADD EMPLOYEE FORM AND EMPLOYEE TABLE FROM THE DOM
+
+// SET A COUNT VARIABLE TO DISPLAY NEXT TO EMPLOYEES HEADER
+
+
+// ADD EMPLOYEE
+
+form.addEventListener('submit', (e) => {
+
+    window.addEventListener("load", () => {
+
+    const $ = id => document.getElementById(id);
+
+    const form = $("employeeForm");
+    const table = $("employees");
+    const count = $("count");
+
+    let employeeCount = 0;
+
+   
+    // PREVENT FORM SUBMISSION
+
+     form.addEventListener("submit", e => {
+        e.preventDefault();
+
+
+    // GET THE VALUES FROM THE TEXT BOXES
+
+        const id = $("id").value;
+        const name = $("name").value;
+        const ext = $("ext").value;
+        const email = $("email").value;
+        const department = $("department").value;
+
+
+    // INSERT A NEW ROW AT THE END OF THE EMPLOYEES TABLE
+
+       const row = table.insertRow();
+
+    // INSERT A CELL FOR EACH ITEM WITHIN THE NEW ROW
+    
+ row.insertCell().appendChild(document.createTextNode(id));
+       
+    // APPEND THE TEXT VALUES AS TEXT NODES WITHIN THE CELLS
+
+        row.insertCell().appendChild(document.createTextNode(name));
+        row.insertCell().appendChild(document.createTextNode(ext));
+        row.insertCell().appendChild(document.createTextNode(email));
+        row.insertCell().appendChild(document.createTextNode(department));
+
+        
+    // CREATE THE DELETE BUTTON
+
+        const deleteCell = row.insertCell();
+        const button = document.createElement("button");
+        button.textContent = "X";
+        button.className = "btn btn-danger btn-sm";
+
+        deleteCell.appendChild(button);
+
+        employeeCount++;
+        count.value = employeeCount;
+
+
+    // RESET THE FORM
+
+  form.reset();
+        $("id").focus();
+    });
+
+
+    // SET FOCUS BACK TO THE ID TEXT BOX
+
+     table.addEventListener("click", e => {
+
+        if (e.target.tagName === "BUTTON") {
+
+            if (confirm("Are you sure you want to delete this employee?")) {
+
+                table.deleteRow(e.target.parentNode.parentNode.rowIndex);
+
+
+    // INCREMENENT THE NUMBER OF EMPLOYEES IN THE TABLE
+
+      employeeCount--;
+                count.value = employeeCount;
+            }
+        }
+
+    });
+
+});
+
+})
+
+// DELETE EMPLOYEE
